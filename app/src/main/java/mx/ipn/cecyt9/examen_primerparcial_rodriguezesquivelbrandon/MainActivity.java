f2340ceb5e4a59c491b1052e4e9e1141f792ea86
Brandon Enrique Rodriguez Esquivel <arakno2010@hotmail.com>
2018-04-12 09:03:01 -0500

    Commit inicial

--- app/src/main/java/mx/ipn/cecyt9/examen_primerparcial_rodriguezesquivelbrandon/MainActivity.java
+++ app/src/main/java/mx/ipn/cecyt9/examen_primerparcial_rodriguezesquivelbrandon/MainActivity.java
@@ -2,12 +2,37 @@ package mx.ipn.cecyt9.examen_primerparcial_rodriguezesquivelbrandon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String usr = "Brandon";
+    String pswd = "asd";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText usuario = findViewById(R.id.usuario);
+        EditText contra = findViewById(R.id.contraseña);
+
+        usr = usuario.getText().toString();
+        pswd = contra.getText().toString();
+
     }
+
+    public void ingresar(View view) {
+        if(usr.equals("Brandon") && pswd.equals("asd")) {
+            Toast toast1 = Toast.makeText(getApplicationContext(),"Usuario incorrecto", Toast.LENGTH_SHORT);
+            toast1.show();
+        }
+        else{
+            Toast toast2 = Toast.makeText(getApplicationContext(),"Usuario Correcto", Toast.LENGTH_SHORT);
+            toast2.show();
+        }
+    }
+
 }
